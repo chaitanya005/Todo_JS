b70bde6b1e2cda3b0c620061de37a3b534426a34
github_chaitanyamuvvala <muvvalachaitanya05@gmail.com>
2019-07-06 19:29:53 +0530

    intial commit

---
+++ ToDO.js
@@ -0,0 +1,23 @@
+
+
+const todo = []
+
+todo.push('WakeUp at 5AM');
+todo.push('Read a Book');
+todo.push('Goto college');
+
+// using forEach loop
+
+// todo.forEach( function(task,index){
+
+//     console.log(`task  ${index+1} is : ${task} `);
+// })
+
+// using for loop
+
+for (let i=0; i<todo.length; i++){
+
+    console.log(todo[i]);
+} 
+
+
